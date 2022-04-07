fcdd74d630471dbc44076ab5384605d8ef0c7e99
Tonny Frisk <tonny035@hotmail.com>
2022-04-07 22:01:54 +0200

    add GameObject entity

---
+++ Atea-code-assignment-api/Entities/GameObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Atea_code_assignment_api.Entities
+{
+    public class GameObject
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public decimal Price { get; set; }
+
+        public GameObject()
+        {
+            Id = Guid.NewGuid().ToString();
+            Price = 0;
+        }
+    }
+}
