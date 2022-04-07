53881a12b8a6aa4c729f2c4c8c245085e40d7809
Tonny Frisk <tonny035@hotmail.com>
2022-04-07 21:38:15 +0200

    create repository

---
+++ Atea-code-assignment-api/Repositories/GameObjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Atea_code_assignment_api.Repositories
+{
+    public class GameObjectRepository : DbContext
+    {
+        public GameObjectRepository(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
