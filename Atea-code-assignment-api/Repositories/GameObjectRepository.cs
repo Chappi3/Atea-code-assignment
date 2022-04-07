007d2e75afe8658e4fb2e6ca180e58c270770e79
Tonny Frisk <tonny035@hotmail.com>
2022-04-07 22:35:27 +0200

    add connectionString & dbContext

--- Atea-code-assignment-api/Repositories/GameObjectRepository.cs
+++ Atea-code-assignment-api/Repositories/GameObjectRepository.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Atea_code_assignment_api.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Atea_code_assignment_api.Repositories
 {
     public class GameObjectRepository : DbContext
     {
+        public DbSet<GameObject> GameObjects { get; set; }
+
         public GameObjectRepository(DbContextOptions options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<GameObject>()
+                .HasIndex(g => g.Name)
+                .IsUnique();
         }
     }
 }
