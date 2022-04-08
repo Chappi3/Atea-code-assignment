4cc5cf33cf86caf057a6360e86cd152c23266be7
Tonny Frisk <tonny035@hotmail.com>
2022-04-08 11:02:33 +0200

    add controller

---
+++ Atea-code-assignment-api/Controllers/GameObjectController.cs
@@ -0,0 +1,34 @@
+﻿using Atea_code_assignment_api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atea_code_assignment_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameObjectController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<string> ListGames(string search)
+        {
+            return search;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> LoadGame(string id)
+        {
+            return id;
+        }
+
+        [HttpPost]
+        public ActionResult<bool> StoreGame([FromBody] GameObject gameObject)
+        {
+            if (gameObject.Id.Equals("ABC-123"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
