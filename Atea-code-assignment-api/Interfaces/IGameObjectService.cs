07fd04548fed96abb96f610910c2d2b2b0ebfead
Tonny Frisk <tonny035@hotmail.com>
2022-04-08 11:18:21 +0200

    add service interface

---
+++ Atea-code-assignment-api/Interfaces/IGameObjectService.cs
@@ -0,0 +1,12 @@
+﻿using Atea_code_assignment_api.Entities;
+using System.Collections.Generic;
+
+namespace Atea_code_assignment_api.Interfaces
+{
+    public interface IGameObjectService
+    {
+        public List<GameObject> ListGames();
+        public GameObject LoadGame(string id);
+        public bool StoreGame(GameObject gameObject);
+    }
+}
